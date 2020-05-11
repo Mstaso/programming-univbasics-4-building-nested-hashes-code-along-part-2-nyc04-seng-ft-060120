c6852c95517253e6a81be491f04982e946d32f79
malcolm staso <mstaso1@gmail.com>
2020-05-11 17:13:12 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -58,7 +58,8 @@ def add_characters
      prince = {:name "Prince Escalus"}
      apothecary = {:name "Apothecary"
      ]
-   
+   epic_tragedy[:additional_characters] << prince
+   epic_tragedy[:additional_characters] << apothecary
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
    # - Add two hashes to this array, both with a :name key.
    #   - The first hash should have the :name key set to "Prince Escalus" 
